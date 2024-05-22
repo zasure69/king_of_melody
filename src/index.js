517f5749d40254209b46a41fb5347a493c29c332
Nguyễn Khắc Hậu <162142037+zasure69@users.noreply.github.com>
2024-05-22 23:12:02 +0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -96,7 +96,7 @@ app.use(passport.session())
 route(app);
 
 
-module.exports = app;
+module.exports = server;
 // app.listen(port, () => {
 //   console.log(`Example app listening on port ${port}`)
 // })

fbfc05bf17ba3a09996a0cab5cab0ea4def4b49e
bachnan <162691927+bachnan@users.noreply.github.com>
2024-05-22 21:09:29 +0700

    Up

---
+++ profile.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const router = express.Router();
+
+const profileController = require('../app/controller/profileController');
+
+const multer = require('multer');
+const upload = multer({ dest: 'uploads/' });
+
+router.get('/', profileController.index);
+router.post('/', upload.single('audioFile'), profileController.update);
+
+module.exports = router;

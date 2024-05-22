d1222dca36149bbcf3f8c84c42c708b86919bf29
Nguyễn Khắc Hậu <22520410@gm.uit.edu.vn>
2024-05-22 23:59:41 +0700

    upload deploy

--- src/routes/profile.js
+++ src/routes/profile.js
@@ -3,15 +3,11 @@ const router = express.Router();
 const path = require('path');
 
 const profileController = require('../app/controller/profileController');
-const fs = require('fs');
+
 const multer = require('multer');
 
-const tmpDir = path.join(__dirname, 'tmp');
-if (!fs.existsSync(tmpDir)) {
-  fs.mkdirSync(tmpDir);
-}
-fs.chmodSync(tmpDir, '755');
-const upload = multer({ dest: '/src/uploads/' });
+
+const upload = multer({ dest: 'uploads/' });
 
 router.get('/', profileController.index);
 router.post('/', upload.single('audioFile'), profileController.update);
